743fa21f3412fce5e7dd0b3168c215f8dc668294
Haf <haf@protonmail.ch>
2022-09-06 17:00:13 +0200

    added a way to specify completions dirs

--- default.nix
+++ default.nix
@@ -4,6 +4,25 @@ with lib;
 
 let
   cfg = config.programs.shell;
+  shellDiscriminatedModule = type: submodule {
+    options = {
+      bash = mkOption {
+        type = nullOr type;
+        description = "bash value for this specific configuration";
+        default = null;
+      };
+      zsh = mkOption {
+        type = nullOr type;
+        description = "zsh value for this specific configuration";
+        default = null;
+      };
+      fish = mkOption {
+        type = nullOr type;
+        description = "fish value for this specific configuratioN";
+        default = null;
+      };
+    };
+  };
 in {
   options.programs.shell = with types; {
     aliases = mkOption {
@@ -17,9 +36,14 @@ in {
       default = null;
       description = "Directory where you hold your scripts, so it gets added to the $PATH";
     };
+
+    completionsDir = mkOption {
+      type = nullOr shellDiscriminatedModule;
+      default = null;
+      description = "Directories where your completion scripts lie";
+    };
   };
 
-  # TODO: Maybe my PATH addition syntax is not ok for all shells?
   # TODO: Make a lib to automate creating this structure, it'll be nice for implementing, for example, thefuck
   config = mkMerge [
     (mkIf (config.programs.bash.enable) {
@@ -30,6 +54,8 @@ in {
           PATH="$PATH:${toString cfg.scriptDir}"
         '';
       };
+
+      home.file.".local/share/bash_completion".target = mkIf (cfg ? completionsDir && cfg.completionsDir ? bash) cfg.completionsDir.bash;
     })
 
     (mkIf (config.programs.fish.enable) {
